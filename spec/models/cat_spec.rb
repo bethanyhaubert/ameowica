7788ab68759bcf3d08346cd43128f08905515258
Beth Haubert <beth@thoughtbot.com>
2019-11-07 11:39:42 -0800

    random changes

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -3,5 +3,10 @@
 require "rails_helper"
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates a cat with an image" do
+    cat = create(:cat)
+    # how to create this file?
+    cat.images.attach(need_to_create_this_file)
+    expect(post.images.any?).to eq(true)
+  end
 end
---
+++ spec/requests/cats_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe "Cats", type: :request do
+  describe "create a new cat" do
+    pending
+  end
+end
