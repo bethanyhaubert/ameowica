93d62e65b3ec0b695dedd98876843868d5c4f8e4
Beth Haubert <beth@thoughtbot.com>
2019-11-07 11:40:44 -0800

    adds views

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,5 +1,28 @@
-class CatController< ApplicationController
+# frozen_string_literal: true
+
+class CatsController < ApplicationController
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    binding.pry
+    @cat.images.attach(image)
+    if @cat.save
+      redirect_to home_path
+    else
+      render :new
+    end
+  end
+
   def show
-    @cat = Cat.find(params['cat_id'])
+    @cat = Cat.find(params[:id])
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :birthday, :image)
   end
 end
